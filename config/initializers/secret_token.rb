c9ecd26417f5531e455ae699c5f7f0de5a33d42e
Robin Doermann <robin@revenflo.com>
2013-07-10 16:36:27 -0400

    Initial reven-commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '4afe387195ebaa828fefac4e8a33ec4ef6114317da6259e02c499ecf19d02b8b50911359a0614e01c5748c299f5738cc281b7f53dc0c4ef80c55cccdcb74a8f3'
